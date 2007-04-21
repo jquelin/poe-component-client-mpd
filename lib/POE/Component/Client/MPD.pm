dee7a48dbba35c71c55cf8e35f079edb81137678
jq <jq@merlin.mongueurs.net>
2007-04-21 17:18:27 +0000

    [project @ 5926] wrote the legalese & description blabla in pod

--- lib/POE/Component/Client/MPD.pm
+++ lib/POE/Component/Client/MPD.pm
@@ -78,7 +78,7 @@ sub _onpriv_start {
     $h->{_socket}  = POE::Component::Client::MPD::Connection->spawn(\%params);
 }
 
-=pod
+=begin FIXME
 
 #
 # _connected()
@@ -97,7 +97,7 @@ sub _got_mpd_version {
     $self->_version($vers);
 }
 
-
+=end FIXME
 
 =cut
 
@@ -135,30 +135,39 @@ sub _onpriv_send {
 
 __END__
 
+
 =head1 NAME
 
-POE::Component::Client::MPD - The great new POE::Component::Client::MPD!
-
-=head1 VERSION
-
-Version 0.01
+POE::Component::Client::MPD - a full-blown mpd client library
 
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+    use POE qw[ Component::Client::MPD ];
+    POE::Component::Client::MPD->spawn(
+        { host     => 'localhost',
+          port     => 6600,
+          password => 's3kr3t',  # mpd password
+          alias    => 'mpd',     # poe alias
+        }
+    );
 
-Perhaps a little code snippet.
+    # ... later on ...
+    $_[KERNEL]->post( 'mpd', 'next' );
 
-    use POE::Component::Client::MPD;
 
-    my $foo = POE::Component::Client::MPD->new();
-    ...
+=head1 DESCRIPTION
 
-=head1 EXPORT
+POE::Component::Client::MPD is a perl mpd client, sitting on top of the POE
+framework.
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+Audio::MPD gives a clear object-oriented interface for talking to and
+controlling MPD (Music Player Daemon) servers. A connection to the MPD
+server is established as soon as a new Audio::MPD object is created.
+Commands are then sent to the server as the class's methods are called.
+
+
+
 
 =head1 FUNCTIONS
 
@@ -168,25 +177,27 @@ if you don't export anything, such as for a purely object-oriented module.
 =head2 function2
 
 
-=head1 AUTHOR
-
-Jerome Quelin, C<< <jquelin at cpan.org> >>
-
 =head1 BUGS
 
-Please report any bugs or feature requests to
-C<bug-poe-component-client-mpd at rt.cpan.org>, or through the web interface at
+Please report any bugs or feature requests to C<bug-poe-component-client-mpd at
+rt.cpan.org>, or through the web interface at
 L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=POE-Component-Client-MPD>.
 I will be notified, and then you'll automatically be notified of progress on
 your bug as I make changes.
 
-=head1 SUPPORT
 
-You can find documentation for this module with the perldoc command.
+=head1 SEE ALSO
 
-    perldoc POE::Component::Client::MPD
+You can find more information on the mpd project on its homepage at
+L<http://www.musicpd.org>, or its wiki L<http://mpd.wikia.com>.
 
-You can also look for information at:
+POE::Component::Client::MPD development takes place on C<< <audio-mpd at
+googlegroups.com> >>: feel free to join us. (use
+L<http://groups.google.com/group/audio-mpd> to sign in). Our subversion
+repository is located at L<https://svn.musicpd.org>.
+
+
+You can also look for information on this module at:
 
 =over 4
 
@@ -202,18 +213,31 @@ L<http://cpanratings.perl.org/d/POE-Component-Client-MPD>
 
 L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=POE-Component-Client-MPD>
 
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/POE-Component-Client-MPD>
-
 =back
+
+
+
+=head1 AUTHOR
+
+Jerome Quelin, C<< <jquelin at cpan.org> >>
 
 
 =head1 COPYRIGHT & LICENSE
 
 Copyright 2007 Jerome Quelin, all rights reserved.
 
-This program is released under the following license: gpl
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 =cut
-
