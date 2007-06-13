6733e2ce22406d8f6560fef2825cb830222f9196
jq <jq@merlin.mongueurs.net>
2007-06-13 08:29:49 +0000

    [project @ 6605]  r5490@merlin:  jquelin | 2007-06-09 16:14:02 +0200  event songs_by_artist_partial handled by $COLLECTION

--- lib/POE/Component/Client/MPD/Collection.pm
+++ lib/POE/Component/Client/MPD/Collection.pm
@@ -23,7 +23,7 @@ Readonly my @EVENTS => qw[
     all_items all_items_simple items_in_dir
     all_albums all_artists all_titles all_files
     song songs_with_filename_partial
-    albums_by_artist songs_by_artist
+    albums_by_artist songs_by_artist songs_by_artist_partial
 ];
 
 sub _spawn {
@@ -249,15 +249,12 @@ sub _onpub_songs_by_artist {
 # Return all AMC::Item::Songs performed by $artist.
 #
 sub _onpub_songs_by_artist_partial {
-    my $what = $_[ARG0];
-    my $msg = POE::Component::Client::MPD::Message->new( {
-        _from      => $_[SENDER]->ID,
-        _request   => $_[STATE],
-        _answer    => $SEND,
-        _commands  => [ qq[search artist "$what"] ],
-        _cooking   => $AS_ITEMS,
-    } );
-    $_[KERNEL]->yield( '_send', $msg );
+    my $msg  = $_[ARG0];
+    my $what = $msg->_params->[0];
+    $msg->_answer   ( $SEND );
+    $msg->_commands ( [ qq[search artist "$what"] ] );
+    $msg->_cooking  ( $AS_ITEMS );
+    $_[KERNEL]->post( $_HUB, '_send', $msg );
 }
 
 
