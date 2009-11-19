727b3d95a253de9a3a915e87e448b0dddad1fc89
Jérôme Quelin <jquelin@gmail.com>
2009-11-19 18:00:21 +0100

    introducing types module

---
+++ lib/POE/Component/Client/MPD/Types.pm
@@ -0,0 +1,70 @@
+use 5.008;
+use warnings;
+use strict;
+
+package POE::Component::Client::MPD::Types;
+# ABSTRACT: types used in the distribution
+
+use Moose::Util::TypeConstraints;
+use Sub::Exporter -setup => { exports => [ qw{
+    Answer Cooking Transform
+} ] };
+
+enum Answer    => qw{ send discard };
+enum Cooking   => qw{ raw as_items as_kv strip_first };
+enum Transform => qw{ as_scalar as_stats as_status };
+
+1;
+__END__
+
+=head1 DESCRIPTION
+
+This module implements the specific types used by the distribution, and
+exports them. It is using L<Sub::Exporter> underneath, so you can use
+all the shenanigans to change the export names.
+
+Current types defined and exported:
+
+=over 4
+
+=item * C<Answer> - a simple enum to know what to do about the message.
+Possible values are:
+
+=over 4
+
+=item * C<send> - send the message back to client
+
+=item * C<discard> - trash mpd's answer
+
+=back
+
+
+=item * C<Cooking> - a simple enum to know what to do about that data
+
+=over 4
+
+=item * C<raw> - data should not be touched
+
+=item * C<as_items> - data is to be transformed as L<Audio::MPD::Common::Item>
+
+=item * C<as_kv> - data is to be cooked as key/values (hash)
+
+=item * C<strip_first> - data should have its first field stripped
+
+=back
+
+
+=item * C<Transform> - a simple enum to know what to do about the data,
+B<after> it has been cooked. Possible values are:
+
+=over 4
+
+=item * C<as_scalar> - return the first element instead of the full list
+
+=item * C<as_stats> - transform the data from key/value to
+C<Audio::MPD::Common::Stats>
+
+=item * C<as_status> - transform the data from key/value to
+C<Audio::MPD::Common::Status>
+
+=back
